7b764f42d3f12d9136fd754aec8c11aeb8775de1
royandugu <royandugu99009@gmail.com>
2023-11-15 09:12:34 +0545

    Skills section

--- components/individual/skills/skills.tsx
+++ components/individual/skills/skills.tsx
@@ -1,11 +1,15 @@
-const Skills=()=>{  
-    return(
-        <main className="pt-32 pl-[7.9%]">
-            <section>
+const Skills = () => {
+    return (
+        <main className="pt-32 pr-32 pl-[7.9%]">
+            <section className="relative">
                 <h1 className="text-[40px] font-bold"> Skills </h1>
-                <div className="mt-10 flex">
-                    <img src="/Images/skillsPic.jpg" className="w-[500px]"/>
-                    <div className="bg-[#ffdc80]"></div>
+                <div className="absolute top-[100px] w-[400px] z-20 border-[10px] border-solid border-white">
+                    <img src="/Images/skillsPic.jpg" className="w-full" />
+                </div>
+                <div className="mt-10 pl-[150px]">
+                    <div className="bg-[#ffdc80] pt-10 flex-1 mt-[100px] h-[500px]">
+                        <h1 className="text-[40px] font-bold"> My Skills </h1>
+                    </div>
                 </div>
             </section>
         </main>
